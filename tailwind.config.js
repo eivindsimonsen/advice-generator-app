dc710571ff1b0bd1bf36378b65696aefc53e8017
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-01-03 17:47:50 +0100

    Desktop design complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
       mobile: "375px",
       desktop: "1440px",
     },
+    fontSize: {
+      primarySize: "28px",
+    },
+    fontFamily: {
+      body: "'Manrope', sans-serif",
+    },
     extend: {
       colors: {
         primaryColor: "hsl(193, 38%, 86%)",
@@ -13,6 +19,9 @@ module.exports = {
         mainBackground: "hsl(217, 19%, 38%)",
         secondBackground: "hsl(217, 19%, 24%)",
         thirdBackground: "hsl(218, 23%, 16%)",
+      },
+      spacing: {
+        translateDice: "-29px",
       },
     },
   },
