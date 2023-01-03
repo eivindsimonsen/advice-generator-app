413dce61bef3879ede17b63beef84a8dea3a337f
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-01-03 01:21:47 +0100

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html/"],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      mobile: "375px",
+      desktop: "1440px",
+    },
+    extend: {
+      colors: {
+        primaryColor: "hsl(193, 38%, 86%)",
+        secondaryColor: "hsl(150, 100%, 66%)",
+        mainBackground: "hsl(217, 19%, 38%)",
+        secondBackground: "hsl(217, 19%, 24%)",
+        thirdBackground: "hsl(218, 23%, 16%)",
+      },
+    },
   },
   plugins: [],
 };
